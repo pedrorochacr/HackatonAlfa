ad7c0ea9662cf521c0553e61c8747e068b3afa85
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2023-08-04 21:58:33 -0300

    started form page

---
+++ externo/src/app/inscricao/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+
+export default function SubscriptionPage() {
+  return (
+    <main className="p-12 space-y-2">
+      <p className="prose">
+        ATENÇÃO: o preenchimento dessas informações é de suma importância para o
+        seu prosseguimento no processo seletivo. Todos os campos são
+        OBRIGATÓRIOS, então se atente às informações preenchidas.
+      </p>
+      <section>
+        <h2 className="text-2xl">Dados Pessoais</h2>
+        <hr />
+        
+      </section>
+    </main>
+  );
+}
