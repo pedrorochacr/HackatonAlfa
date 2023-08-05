c420924cbea395e0f1a3818757d73cacdfe33ec4
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2023-08-04 22:11:32 -0300

    .

--- externo/src/app/inscricao/page.tsx
+++ externo/src/app/inscricao/page.tsx
@@ -1,8 +1,27 @@
 'use client';
 
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const schema = z.object({
+  nomeCompleto: z.string().min(3).max(255),
+  nomeCompletoMae: z.string().min(3).max(255),
+  nomeCompletoPai: z.string().min(3).max(255),
+});
+
+type FormValues = z.infer<typeof schema>;
 
 export default function SubscriptionPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = (data: FormValues) => console.log(data);
+
   return (
     <main className="p-12 space-y-2">
       <p className="prose">
@@ -10,11 +29,14 @@ export default function SubscriptionPage() {
         seu prosseguimento no processo seletivo. Todos os campos são
         OBRIGATÓRIOS, então se atente às informações preenchidas.
       </p>
-      <section>
-        <h2 className="text-2xl">Dados Pessoais</h2>
-        <hr />
-        
-      </section>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
+        <section>
+          <h2 className="text-2xl">Dados Pessoais</h2>
+          <hr />
+          <input {...register('nomeCompleto')} />
+          <p>{errors.nomeCompleto?.message}</p>
+        </section>
+      </form>
     </main>
   );
 }
